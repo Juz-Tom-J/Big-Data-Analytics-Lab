02c355e3c82de58664691384dbb2dcb7b51ec859
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2023-04-10 19:25:20 +0530

    Code added

--- Logistic Regression.R
+++ Logistic Regression.R
@@ -1,3 +1,5 @@
+install.packages("caret")
+
 library(MASS)
 library(caret)
 data(biopsy)
@@ -5,11 +7,13 @@ data(biopsy)
 f1 <- as.integer(readline("Enter the value for feature 1: "))
 f2 <- as.integer(readline("Enter the value for feature 2: "))
 f3 <- as.integer(readline("Enter the value for feature 3: "))
+
 test <- data.frame(V1=f1,V2=f2,V3=f3)
 
 set.seed(123)
 
 biopsy_model <- glm(class~V1+V2+V3,data=biopsy,family="binomial")
 pred <- predict(biopsy_model,newdata=test,type="response")
+
 res <- ifelse(pred>0.5,"malignant","benign")
 cat("The tumor is",res,"\n")
