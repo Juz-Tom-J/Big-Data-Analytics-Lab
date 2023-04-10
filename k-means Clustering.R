7d3971e0d3c4c3dacda2ed76270c28a713b5133a
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2023-04-10 19:22:48 +0530

    Code added

---
+++ k-means Clustering.R
@@ -0,0 +1,10 @@
+library(cluster)
+library(ggplot2)
+
+data(iris)
+
+sub <- subset(iris, select = -c(Species))
+
+k_means_model <- kmeans(sub, centers = 3)
+
+print(autoplot(k_means_model, sub, frame = TRUE))
