e6affe3ff6d2950185426d1ba12fff784128dc69
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2023-04-10 19:44:00 +0530

    Code added

--- k-means Clustering.R
+++ k-means Clustering.R
@@ -1,13 +1,19 @@
 install.packages("cluster")
+install.packages("dplyr")
 install.packages("ggplot2")
+install.packages("ggfortify")
 
 library(cluster)
+library(dplyr)
 library(ggplot2)
+library(ggfortify)
 
 data(iris)
 
 sub <- subset(iris, select = -c(Species))
 
-k_means_model <- kmeans(sub, centers = 3)
+k <- 3
+k_means_model <- kmeans(sub, centers = k)
 
-print(autoplot(k_means_model, sub, frame = TRUE))
+graph <- autoplot(k_means_model, sub, frame = TRUE)
+print(graph)
