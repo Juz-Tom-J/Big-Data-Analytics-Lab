f453c830cc6522b826a3d1fae998b3b8a06fb8bb
Justin-Thomas-Jo <98319250+Juz-Tom-J@users.noreply.github.com>
2023-04-18 19:39:03 +0530

    Code added

---
+++ Statistical Tests.R
@@ -0,0 +1,42 @@
+data(mtcars)
+data(iris)
+
+one_sample = t.test(mtcars$mpg, mu=20)
+print(one_sample)
+
+two_sample = t.test(mpg~cyl, data = mtcars, subset = cyl %in% c(4,6))
+print(two_sample)
+
+paired = t.test(mtcars$mpg, mtcars$wt,data = mtcars, paired = TRUE)
+print(paired)
+
+anova = aov(mpg~hp,data=mtcars)
+print(anova)
+
+norm = shapiro.test(mtcars$wt)
+print(norm)
+
+arb_dist = ks.test(mtcars$disp,mtcars$gear)
+print(arb_dist)
+
+wilcx = wilcox.test(iris$Petal.Width, mu = 4)
+print(wilcx)
+
+krusk = kruskal.test(Petal.Length~Species, data=iris)
+print(krusk)
+
+flig_kill = fligner.test(Sepal.Width~Species, data=iris)
+print(flig_kill)
+
+ans_brad = ansari.test(iris$Petal.Length,iris$Petal.Width)
+print(ans_brad)
+
+x = 64
+n = 100
+p = 0.60
+
+one_prop = prop.test(x,n,p)
+print(one_prop)
+
+binom = binom.test(x,n,p=0.5)
+print(binom)
